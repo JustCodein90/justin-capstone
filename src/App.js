aff90d94dcb121e40d3a8b2973d7de37701fceab
Justin Ireland <143423020+JustCodein90@users.noreply.github.com>
2023-11-19 16:24:51 -0500

    revised capstone using file structure Layouts and pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './Componets/Forms/Button';
+import { HomeNav } from './Componets/Layout/Navbar/HomeNav';
 
 function App() {
   return (
+    
     <div className="App">
+      <HomeNav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button />
         <a
           className="App-link"
           href="https://reactjs.org"
